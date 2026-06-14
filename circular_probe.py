1c9a3eee95c1fbe7e3f69afc9a77fe1613fc2684
Andrea Chen <andreayhchen@gmail.com>
2026-06-13 19:56:12 -0700

    more circular probing

--- circular_probe.py
+++ circular_probe.py
@@ -10,6 +10,8 @@ from transformer_lens import (
 from transformer_lens.hook_points import HookPoint
 from datasets import load_dataset
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
 
 MODEL_NAME = "google/gemma-2-2b"
 LAYERS = [1, 5, 9, 13, 17, 21, 25]
@@ -35,3 +37,36 @@ for l in LAYERS:
 day_order = [" Monday", " Tuesday", " Wednesday", " Thursday", " Friday", " Saturday", " Sunday"]
 labels = [day_order.index(days_map[tid.item()]) for tid in day_ids_after]
 
+pca_embds = PCA(n_components=5).fit(day_embds_after.float().numpy())
+Z_embds = pca_embds.transform(day_embds_after.float().numpy())
+
+pca = {}
+Z = {}
+for l in LAYERS:
+    pca[l] = PCA(n_components=5).fit(day_h_after[l].float().numpy())
+    Z[l] = pca[l].transform(day_h_after[l].float().numpy())
+
+targets = np.array([[np.cos(i * 2 * np.pi / 7), np.sin(i * 2 * np.pi / 7)] for i in labels])
+
+
+_, residuals_embds, _, _ = np.linalg.lstsq(Z_embds, targets, rcond=None)
+residuals = {}
+for l in LAYERS:
+    _, residuals[l], _, _ = np.linalg.lstsq(Z[l], targets, rcond=None)
+
+mse_embds = residuals_embds.sum()/len(labels)
+mse_layers = {l: residuals[l].sum()/len(labels) for l in LAYERS}
+
+# plot 
+x = [0] + LAYERS
+y = [mse_embds] + [mse_layers[l] for l in LAYERS]
+
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.plot(x, y, marker="o")
+ax.set_xlabel("Layer (0 = embedding)")
+ax.set_ylabel("Circular probe MSE")
+ax.set_title("Circularity of day-of-week representations across layers")
+ax.set_xticks(x)
+plt.tight_layout()
+plt.savefig("/workspace/circular_probe_loss.png", dpi=150)
+log("Saved circular_probe_loss.png")
